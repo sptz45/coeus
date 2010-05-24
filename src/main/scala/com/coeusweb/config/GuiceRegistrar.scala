92a15e08abed97a8e74a6290030aed96827f05b4
Spiros Tzavellas <spiros@tzavellas.com>
2010-05-24 08:49:23 +0300

    Added copyright info in new files.

--- src/main/scala/com/coeusweb/config/GuiceRegistrar.scala
+++ src/main/scala/com/coeusweb/config/GuiceRegistrar.scala
@@ -1,3 +1,9 @@
+/* - Coeus web framework -------------------------
+ *
+ * Licensed under the Apache License, Version 2.0.
+ *
+ * Author: Spiros Tzavellas
+ */
 package com.coeusweb.config
 
 import scala.collection.JavaConversions._
--- src/main/scala/com/coeusweb/core/DispatcherContextLoader.scala
+++ src/main/scala/com/coeusweb/core/DispatcherContextLoader.scala
@@ -1,3 +1,9 @@
+/* - Coeus web framework -------------------------
+ *
+ * Licensed under the Apache License, Version 2.0.
+ *
+ * Author: Spiros Tzavellas
+ */
 package com.coeusweb.core
 
 import java.lang.reflect.InvocationTargetException 
