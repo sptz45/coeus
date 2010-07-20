a945e633bc49bb90c86666590a538c28f3e1c154
Spiros Tzavellas <spiros@tzavellas.com>
2010-07-20 23:45:59 +0300

    Added implementation note.

--- src/main/scala/com/coeusweb/validation/vspec/constraint/Constraint.scala
+++ src/main/scala/com/coeusweb/validation/vspec/constraint/Constraint.scala
@@ -37,4 +37,22 @@ trait Constraint[-T] {
    * @see Error
    */
   def getError(targetClass: Class[_], field: String, value: T): Error
+  
+  /* Why Constraint needs to have the getError(..) method?
+   * 
+   * You might think that the error construction is a separate concern from
+   * constraint validation and thus by having the getError method this trait
+   * violates the single responsibility principle. The getError method needs
+   * to be on this trait because sometimes the returned Error must contain
+   * information specific to the Constraint instance and since that instance
+   * is the only object that knows that information it also has to construct
+   * the Error.
+   * 
+   * For example in a constraint that we test whether a number is
+   * greater that another number (minimum) we might want to include the
+   * minimum number in the Error instance so it can be used in the error
+   * messages. That number will probably be know only by the Constraint
+   * instance that will validate the input number (see:
+   * NumericConstraints.isGreaterThan(..)).
+   */
 }
