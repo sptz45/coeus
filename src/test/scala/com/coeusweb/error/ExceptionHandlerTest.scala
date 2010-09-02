34657a4fa2484985d4a4992118b23832397f6331
Spiros Tzavellas <spiros@tzavellas.com>
2010-09-02 09:29:46 +0300

    Improved ExceptionHandler's tests.

--- src/test/scala/com/coeusweb/error/ExceptionHandlerTest.scala
+++ src/test/scala/com/coeusweb/error/ExceptionHandlerTest.scala
@@ -16,27 +16,40 @@ class ExceptionHandlerTest {
     new core.RequestContext(req, res, null)
   }
   
-  val missingParam = render("client-error")
+  val missingParamView = render("client-error")
   
   val handler = ExceptionHandler.forServlet("test-servlet") {
-    case e: MissingParameterException => missingParam
+    case e: MissingParameterException => missingParamView
   }
   
   @Test
-  def return_a_view_from_the_partial_function() {
+  def the_default_handler_always_returns_ErrorPageView() {
+    val defaultHandler = ExceptionHandler.defaultHandler("test-servlet")
+    context.error = new RuntimeException
+    val errorView = defaultHandler.handle(context)
+    
+    assertEquals(ErrorPageView, errorView)
+    assertEquals(500, context.response.status)
+    assertEquals(context.error, context.request("exception"))
+  }
+  
+  @Test
+  def the_handler_returns_a_view_from_the_partial_function() {
     context.error = new MissingParameterException("q")
     val errorView = handler.handle(context)
     
-    assertEquals(missingParam, errorView)
+    assertEquals(missingParamView, errorView)
     assertEquals(400, context.response.status)
+    assertEquals(context.error, context.request("exception"))
   }
   
   @Test
-  def the_default_error_view_is_ErrorPageView() {
+  def the_handler_returns_ErrorPageView_if_no_view_found_in_partial_function() {
     context.error = new IllegalStateException
     val errorView = handler.handle(context)
     
     assertEquals(ErrorPageView, errorView)
     assertEquals(500, context.response.status)
+    assertEquals(context.error, context.request("exception"))
   }
 }
