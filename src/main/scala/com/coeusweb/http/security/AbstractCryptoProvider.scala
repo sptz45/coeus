c9614829c95ffec30b5fd5a3d5e6f2c440df1cab
Spiros Tzavellas <spiros@tzavellas.com>
2010-10-18 12:18:48 +0300

    Added file header.

--- src/main/scala/com/coeusweb/http/security/AbstractCryptoProvider.scala
+++ src/main/scala/com/coeusweb/http/security/AbstractCryptoProvider.scala
@@ -1,3 +1,9 @@
+/* - Coeus web framework -------------------------
+ *
+ * Licensed under the Apache License, Version 2.0.
+ *
+ * Author: Spiros Tzavellas
+ */
 package com.coeusweb.http.security
 
 import java.security.{ MessageDigest, SignatureException }
